2d94080e82a1950cabf9819be847af71884f4c37
jalpeshindrodiya <108825834+jalpeshindrodiya@users.noreply.github.com>
2022-07-18 19:12:38 +0530

    add minimal api

--- WebReminderApp/Program.cs
+++ WebReminderApp/Program.cs
@@ -1,4 +1,9 @@
+using WebReminderApp.Database;
+using WebReminderApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton<IReminderService, ReminderService>();
 
 // Add services to the container.
 builder.Services.AddRazorPages();
@@ -23,5 +28,56 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.MapControllers();
+
+app.MapGet("/api/RemenderList", (IReminderService service) =>
+{
+    return Results.Ok(service.ReminderList());
+});
+
+app.MapGet("/api/Detail",(int id,IReminderService service) =>
+{
+    return Results.Ok(service.GetDetail(id));
+});
+
+app.MapPost("/api/Create", (HttpRequest Request, IReminderService service) =>
+{
+    var Iscompleted = false;
+    if (Request.Form["isCompleted"].Equals("on"))
+    {
+        Iscompleted = true;
+    }
+    Reminders reminders = new()
+    {
+        Name = Request.Form["name"],
+        Title = Request.Form["title"],
+        Description = Request.Form["description"],
+        DueDate = DateTime.Parse(Request.Form["dueDate"]),
+        IsCompleted = Iscompleted
+    };
+    return Results.Ok(service.Post(reminders));
+});
+
+app.MapPost("/api/Edit", (HttpRequest Request,IReminderService service) =>
+{
+    var Iscompleted = false;
+    if (Request.Form["isCompleted"].Equals("on"))
+    {
+        Iscompleted = true;
+    }
+    Reminders reminders = new()
+    {
+        Name = Request.Form["name"],
+        Title = Request.Form["title"],
+        Description = Request.Form["description"],
+        DueDate = DateTime.Parse(Request.Form["dueDate"]),
+        IsCompleted = Iscompleted
+    };
+    return Results.Ok(service.Edit(reminders));
+});
+app.MapGet("/api/Delete", (int id,IReminderService service) =>
+{
+    return Results.Ok(service.Delete(id));
+});
+
 
 app.Run();
