5ed55a410c9c9c972affe542c4ad15468e199029
jalpeshindrodiya <108825834+jalpeshindrodiya@users.noreply.github.com>
2022-07-18 19:09:25 +0530

    changes in index page

--- WebReminderApp/Pages/Index.cshtml.cs
+++ WebReminderApp/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebReminderApp.Controllers;
+using WebReminderApp.Database;
+using WebReminderApp.Services;
 
 namespace WebReminderApp.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IReminderService _reminderService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger,IReminderService service)
         {
             _logger = logger;
+            _reminderService = service;
         }
 
         public void OnGet()
         {
+            var reminderlist = _reminderService.ReminderList();
 
+            ViewData["RemindersData"] = reminderlist;
         }
     }
 }
