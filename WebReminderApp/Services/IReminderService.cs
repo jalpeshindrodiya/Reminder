5f3a8fb896d777aec50e59d7181b338498561c5c
jalpeshindrodiya <108825834+jalpeshindrodiya@users.noreply.github.com>
2022-07-18 19:10:22 +0530

    add interface and service

---
+++ WebReminderApp/Services/IReminderService.cs
@@ -0,0 +1,17 @@
+﻿using WebReminderApp.Database;
+
+namespace WebReminderApp.Services
+{
+    public interface IReminderService
+    {
+        List<Reminders> ReminderList();
+
+        Reminders GetDetail(int id);
+
+        Reminders Post(Reminders reminders);
+
+        Reminders Edit(Reminders reminders);
+
+        Reminders Delete(int id);
+    }
+}
---
+++ WebReminderApp/Services/ReminderService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebReminderApp.Database;
+
+namespace WebReminderApp.Services
+{
+    public class ReminderService : IReminderService
+    {
+        public List<Reminders> ReminderList()
+        {
+            using (var db = new DataContext())
+            {
+                return db.Reminders.ToList();
+            }
+        }
+
+        // GET api/<ReminderController>/5
+        public Reminders GetDetail(int id)
+        {
+            using (var db = new DataContext())
+            {
+                Reminders reminder = db.Reminders.FirstOrDefault(x => x.Id == id);
+                return reminder;
+            }
+
+        }
+
+        public Reminders Post(Reminders reminders)
+        {
+            using (var db = new DataContext())
+            {
+                db.Reminders.Add(reminders);
+                db.SaveChanges();
+            }
+            return reminders;
+        }
+        public Reminders Edit(Reminders reminders)
+        {
+            using (var db = new DataContext())
+            {
+                db.Entry(reminders).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return reminders;
+        }
+
+        // PUT api/<ReminderController>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        public Reminders Delete(int id)
+        {
+            using (var db = new DataContext())
+            {
+                Reminders reminders = db.Reminders.Find(id);
+                if (reminders == null)
+                {
+                    //return 1;
+                }
+                else
+                {
+                    db.Reminders.Remove(reminders);
+                    db.SaveChanges();
+                }
+                return reminders;
+            }
+        }
+    }
+}
